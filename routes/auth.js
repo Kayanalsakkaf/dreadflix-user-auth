b5c95f8531486a4b079b3bf3de06aaba0771fd08
Kayan <kayanalsakkaf@outlook.com>
2025-02-25 13:21:55 +0000

    Update status message in auth route response

--- routes/auth.js
+++ routes/auth.js
@@ -57,7 +57,7 @@ router.post("/login", async (req, res) => {
 });
 
 router.get("/", async (req, res) => {
-  res.status(200).json({ status: "Service Xox" });
+  res.status(200).json({ status: "Service running" });
 });
 
 module.exports = router;
